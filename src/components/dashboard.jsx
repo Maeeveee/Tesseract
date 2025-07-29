87697b34ec370b869a5d5529027a756dbb541884
Maeeveee <142636445+Maeeveee@users.noreply.github.com>
2025-07-29 10:49:06 +0700

    fix dashboard layout

--- src/components/dashboard.jsx
+++ src/components/dashboard.jsx
@@ -7,31 +7,33 @@ export default function Dashboard() {
     return (
         <div className="p-4 sm:ml-64">
             <div className="border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14">
-                <div className='flex flex-row'>
-                    <div className="flex justify-start items-start my-7 ml-7" style={{ overflow: 'visible' }}>
-                    <ProfileCard
-                        name="Rizal Abrar Fahmi"
-                        title="Raja React"
-                        handle="Maeve"
-                        status="Online"
-                        contactText="Contact Me"
-                        avatarUrl="/avatar.png"
-                        showUserInfo={true}
-                        enableTilt={true}
-                        enableMobileTilt={false}
-                        onContactClick={() => console.log('Contact clicked')}
-                        className="pc-card--small" 
-                    />
-                </div>
-                <div className='flex-1 flex flex-col items-center justify-center my-7'>
-                    <h1 className="text-4xl font-bold text-amber-50 text-center mb-4">Wassup Bro</h1>
-                    <p className="ml-6 text-gray-400">through this website i want to show u my <span className='underline text-gray-200'>grand</span> <span className='underline text-gray-200'> design</span>. which is a <span className='underline text-gray-200'>portofolio</span> and also a <span className='underline text-gray-200'>kanban</span> and also a <span className='underline text-gray-200'>notes</span> and also a <span className='underline text-gray-200'>playlist</span> and also a <span className='underline text-gray-200'>game</span>  . And i want to share all my project i have done here. </p>
-                </div>
-                <span className='flex items-end justify-end text-gray-400 m-4'>look down there</span>
+                <div className="flex flex-col md:flex-row">
+                    <div className="flex justify-center items-center my-7 md:ml-7 w-full md:w-auto">
+                        <ProfileCard
+                            name="Rizal Abrar Fahmi"
+                            title="Raja React"
+                            handle="Maeve"
+                            status="Online"
+                            contactText="Contact Me"
+                            avatarUrl="/avatar.png"
+                            showUserInfo={true}
+                            enableTilt={true}
+                            enableMobileTilt={false}
+                            onContactClick={() => console.log('Contact clicked')}
+                            className="pc-card--small"
+                        />
+                    </div>
+                    <div className="flex-1 flex flex-col items-center justify-center my-7">
+                        <h1 className="text-4xl font-bold text-amber-50 text-center mb-4">Wassup Bro</h1>
+                        <p className="text-gray-400 text-center md:ml-6 md:text-left p-3 ">
+                            through this website i want to show u my <span className='underline text-gray-200'>grand</span> <span className='underline text-gray-200'> design</span>. which is a <span className='underline text-gray-200'>portofolio</span> and also a <span className='underline text-gray-200'>kanban</span> and also a <span className='underline text-gray-200'>notes</span> and also a <span className='underline text-gray-200'>playlist</span> and also a <span className='underline text-gray-200'>game</span> . And i want to share all my project i have done here.
+                        </p>
+                    </div>
+                <span className='hidden lg:flex items-end justify-end text-gray-400 m-4'>look down there</span>
                 </div>
             </div>
             <IntroSection />
             <ProjectSection />
-        </div>
+        </div >
     )
 }
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -22,7 +22,7 @@ export default function Navbar() {
                         <div>
                             <button type="button" class="flex text-sm bg-gray-800 rounded-full focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" aria-expanded="false" data-dropdown-toggle="dropdown-user">
                                 <span class="sr-only">Open user menu</span>
-                                <img class="w-8 h-8 rounded-full" src="https://flowbite.com/docs/images/people/profile-picture-5.jpg" alt="user photo"></img>
+                                <img class="w-8 h-8 rounded-full" src="/avatar.png" alt="user photo"></img>
                             </button>
                         </div>
                         <div class="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-sm shadow-sm dark:bg-gray-700 dark:divide-gray-600" id="dropdown-user">

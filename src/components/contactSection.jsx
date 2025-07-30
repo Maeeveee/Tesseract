6d8fbc7d549579a141b982bc0de02d7755ae6782
Maeeveee <142636445+Maeeveee@users.noreply.github.com>
2025-07-30 10:59:30 +0700

    contact text

--- src/components/contactSection.jsx
+++ src/components/contactSection.jsx
@@ -86,8 +86,7 @@ export default function ContactSection() {
         <section className="border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14">
             <div className="p-6">
                 <h1 className="text-4xl text-white font-bold mb-4">Where u can find me</h1>
-                <p className="text-md lg:text-xl text-gray-400 mb-8">
-                    A passage to the majestic <span className="font-semibold text-gray-200">Raja React</span>.
+                <p className="text-md lg:text-xl text-gray-400 mb-8">Your direct line to His Majesty <span className="font-semibold text-gray-200">Raja React</span>.
                 </p>
                 <ul className="grid grid-cols-2 md:grid-cols-3 gap-4">
                     {contacts.map((c, i) => (
